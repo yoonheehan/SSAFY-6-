96daae1075de55e1300074ab85e5fcd12a9fbb44
youngmin <kgjdudals@naver.com>
2022-01-26 15:03:03 +0900

    detail 페이지

--- front/src/components/detail/components/ContentDetail.jsx
+++ front/src/components/detail/components/ContentDetail.jsx
@@ -69,8 +69,28 @@ const VersusRight = styled.b`
   font-size: 30px;
 `
 
+const VersusO = styled.div`
+  font-size: 50px;
+  font-weight: bold;
+`
+
+const VersusX = styled.div`
+  font-size: 50px;
+  font-weight: bold;
+`
+
+const SelectWrapped = styled.div`
+  margin: 10px auto 10px auto;
+`
+const Select = styled.select``
+
+const SelectBtn = styled.button``
+
+const SelectOption = styled.option``
 
 function ContentDetail(props) {
+
+
     return (
         <Base>
             <Profile>
@@ -85,18 +105,50 @@ function ContentDetail(props) {
                 <br />
                 <ContentImg src="/images/1.jpg" alt="게시글이미지"/>
             </ContentContainer>
-            <VersusWrapped>
-                <VersusLeft>치킨</VersusLeft>
-                <VersusImg src="/images/VsImg.png" />
-                <VersusRight>피자</VersusRight>
-            </VersusWrapped>
-            <Progressbar 
-            bgcolor="red" 
-            bgcolor2="blue" 
-            progress="30" 
-            height={30}
-            versus1='콜라' 
-            versus2='피자' />
+            
+            {props.selectType === 1 &&
+              <div>
+                <VersusWrapped>
+                    <VersusLeft>치킨</VersusLeft>
+                    <VersusImg src="/images/VsImg.png" />
+                    <VersusRight>피자</VersusRight>
+                </VersusWrapped>
+                <Progressbar 
+                bgcolor="red" 
+                bgcolor2="blue" 
+                progress="30" 
+                height={30}
+                versus1='콜라' 
+                versus2='피자' />
+              </div>
+            }
+
+            {props.selectType === 2 &&
+              <div>
+                <VersusWrapped>
+                    <VersusO>O</VersusO>
+                    <VersusImg src="/images/VsImg.png" />
+                    <VersusX>X</VersusX>
+                </VersusWrapped>
+              </div>
+            }
+
+            {props.selectType === 3 &&
+              <div>
+                <SelectWrapped>
+                  <Select>
+                      <SelectOption value="">항목을 선택해주세요.</SelectOption>
+                      <SelectOption value='치킨'>치킨</SelectOption>
+                      <SelectOption value='피자'>피자</SelectOption>
+                      <SelectOption value='짬뽕'>짬뽕</SelectOption>
+                  </Select>
+                  <SelectBtn>선택하기</SelectBtn>
+                  <hr />
+                </SelectWrapped>
+              </div>
+            }
+
+
         </Base>
     )
 }
