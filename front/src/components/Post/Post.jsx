117ece352696ef60e429871fd00465d3f46a128e
정정채 <jeong_3150@naver.com>
2022-02-04 14:05:52 +0900

    post tag기능 추가

--- front/src/components/Post/Post.jsx
+++ front/src/components/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useCallback} from "react"
+import React, {useState, useRef, useCallback, useEffect} from "react"
 import { useSelector } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -20,8 +20,12 @@ const Post = () => {
 	// {id: 1, value: inputPlus()}
 	const [selected, setSelected] = useState(false)
 	const [keySelected, setKeySelected] = useState(null)
+	const [hashtag, setHashtag] = useState('')
+	const [isErrored, setIsErrored] = useState(false)
+	const [hashArr, setHashArr] = useState([])
 	const [votes, setVote] = useState([{id: 0, value: inputPlus()}])
 	const nextId = useRef(1)
+	const ref = useRef(null)
 
 	const addEvent = useCallback(
 		(event) => {
@@ -37,6 +41,60 @@ const Post = () => {
 	const removeEvent = (event, props) => {
 		setVote(votes.filter(item => item.id !== props.id))
 		event.preventDefault()
+	}
+
+	const changeHashtag = (event) => {
+		return (
+			setHashtag(event.target.value)
+		)
+	} 
+
+	const inputHashtag = (event) => {
+		if (event.keyCode === 13 || event.type === 'click') {
+			setIsErrored(false)
+			if (hashtag.length > 0) {
+				if (hashArr.length <= 4) {
+					if (hashArr.indexOf(event.target.value) === -1) {
+						if (event.target.value.length <= 10) {
+							return (
+								setHashArr(oldArray => [...oldArray, hashtag]),
+								setHashtag('')
+							)
+						} else {
+							setIsErrored(true)
+							console.log("최대 길이 10자")
+						}
+					} else {
+						setIsErrored(true)
+						console.log("중복됩니다")
+					}
+				} else {
+					setIsErrored(true)
+					console.log("최대 개수 5개")
+				}
+			}
+		} 		
+	}
+
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (isErrored && ref.current && !ref.current.contains(event.target)) {
+				setIsErrored(false)
+			}
+		}
+	
+		document.addEventListener("mousedown", handleClickOutside)
+	
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside)
+		}
+	}, [isErrored])
+
+	const removeHashtag = (event, value) => {
+		const tempArr = hashArr.filter(tag => tag !== value)
+		return (
+			setHashArr(tempArr)
+		)
 	}
 
 	return (
@@ -109,6 +167,7 @@ const Post = () => {
 										</div>	
 									</div>
 								</form>
+								<input type="file" />
 							</div>
 						</div>
 						<div className="my_accordion">
@@ -131,6 +190,44 @@ const Post = () => {
 						</div>
 					</div>				
 				</div>
+				<div className="title mt-4">
+					<div>#태그 입력</div>
+				</div>
+				<div className="hashtag_outer">
+					{hashArr.map((value, key) => (
+						<div key={key}>
+							<div className="hashtag_inner">
+								<div>{value}</div>
+								<div 
+									style={{marginLeft: '5px'}}
+									className="hashtag_remove"
+									onClick={(event) => removeHashtag(event, value)}
+								>
+									<i class="bi bi-x-lg"></i>
+								</div>
+							</div>
+							
+						</div>
+					))}
+				</div>
+				<div className="input_area">
+					<input
+						type="text"
+						value={hashtag}
+						placeholder="해시태그를 입력하세요."
+						onChange={changeHashtag}
+						onKeyUp={inputHashtag}
+						className="hashtag_input"
+						autoFocus
+					/>
+					<button onClick={inputHashtag} className="input_button btn btn-primary btn-sm">+</button>
+				</div>
+				<div className={isErrored ? "bubble active" : "bubble"} ref={ref}>
+					<ul style={{margin: 0}}>
+						<li>태그의 길이는 10자 이하로 작성합니다.</li>
+						<li>최대 5개의 태그를 입력할 수 있습니다.</li>
+					</ul>
+				</div>
 				<div className="mt-4" align="right">
 					<form action="">
 						<Button variant="primary">작성</Button>
@@ -138,10 +235,6 @@ const Post = () => {
 				</div>
 			</div>
 		</>
-		
-		
-		
-
 	)
 }
 
