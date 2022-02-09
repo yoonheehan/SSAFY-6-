481c1da719bc690a9122a6f4d68eeab907356814
swchae516 <swchae516@gmail.com>
2022-02-09 14:26:49 +0900

    이미지 업로드 & S3 연동 & CSS 수정

--- front/src/components/profile/mdProfile.jsx
+++ front/src/components/profile/mdProfile.jsx
@@ -10,7 +10,6 @@ import {
 import styles from './mdProfile.module.css';
 import { BsPersonCircle } from 'react-icons/bs';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { useHistory } from 'react-router-dom';
 
 const MdProfile = props => {
   const inputRef = useRef();
@@ -19,7 +18,7 @@ const MdProfile = props => {
   AWS.config.update({
     region: 'ap-northeast-2', // 버킷이 존재하는 리전을 문자열로 입력합니다. (Ex. "ap-northeast-2")
     credentials: new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: 'ap-northeast-2:e812a597-ca5d-4e15-bb00-dbf788e31f86', // cognito 인증 풀에서 받아온 키를 문자열로 입력합니다. (Ex. "ap-northeast-2...")
+      IdentityPoolId: process.env.REACT_APP_S3, // cognito 인증 풀에서 받아온 키를 문자열로 입력합니다. (Ex. "ap-northeast-2...")
     }),
   });
   const handleFileInput = e => {
@@ -48,10 +47,6 @@ const MdProfile = props => {
       }
     );
   };
-  const history = useHistory();
-  if (localStorage.getItem('loginedUser') === null) {
-    history.push('/')
-  }
   return (
     <>
       <div className={styles.div1}>
@@ -64,30 +59,53 @@ const MdProfile = props => {
       <section className={styles.section}>
         <div className={styles.body}>
           <div className={styles.data}>
-            <input
-              type="file"
-              id="upload"
-              className={styles.image_upload}
-              onChange={handleFileInput}
-            />
-            <label htmlFor="upload" className={styles.image_upload_wrapper}>
-              <div style={{ border: '1px solid black' }}>
-                <img
-                  style={{ width: '100%', height: '100%' }}
-                  ref={imgRef}
-                  src={
-                    'https://haejwoing.s3.ap-northeast-2.amazonaws.com/' +
-                    img +
-                    '.jpg'
-                  }
-                  alt="ㅇ"
-                  onError={() => {
-                    return (imgRef.current.src =
-                      'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png');
-                  }}
+            <div style={{ textAlign: 'center' }}>
+              <div
+                style={{
+                  width: '200px',
+                  height: '200px',
+                  borderRadius: '50%',
+                  display: 'inline-block',
+                  marginBottom: '20px',
+                }}
+              >
+                <input
+                  type="file"
+                  id="upload"
+                  onChange={handleFileInput}
+                  style={{ display: 'none' }}
                 />
+                <label
+                  style={{
+                    marginBottom: '50px',
+                    width: '200px',
+                    height: '200px',
+                    borderRadius: '50%',
+                    cursor: 'pointer',
+                  }}
+                  htmlFor="upload"
+                >
+                  <img
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      borderRadius: '50%',
+                    }}
+                    ref={imgRef}
+                    src={
+                      'https://haejwoing.s3.ap-northeast-2.amazonaws.com/' +
+                      img +
+                      '.jpg'
+                    }
+                    alt="ㅇ"
+                    onError={() => {
+                      return (imgRef.current.src =
+                        'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png');
+                    }}
+                  />
+                </label>
               </div>
-            </label>
+            </div>
             <InputGroup className={styles.inputGroup}>
               <FormControl
                 aria-label="Default"
