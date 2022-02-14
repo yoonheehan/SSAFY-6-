70ad07a3ba37194821ddfb4d0eee67eb47929d5a
jaehyun <jungjh122@naver.com>
2022-02-14 13:37:59 +0900

    jaehyun

--- back/src/main/java/com/haejwoing/back/model/mapper/BoardMapper.java
+++ back/src/main/java/com/haejwoing/back/model/mapper/BoardMapper.java
@@ -27,12 +27,10 @@ public interface BoardMapper {
 
     List<Integer> getIdx(int boardId);
 
-<<<<<<< Updated upstream
     void update_board_vote_num(HashMap temporary);
 
     int get_user_id_from_board(int boardid);
-=======
+
     List<Board> getFollowerFeed(List<Integer> list);
->>>>>>> Stashed changes
 
 }
